bb55b5df7305fe97374429a1199248e167ec5475
happycatdog <104048039+happycatdog@users.noreply.github.com>
2022-05-09 11:26:41 +0900

    Add files via upload

---
+++ Sample707/h/component1.h
@@ -0,0 +1,11 @@
+#ifndef _COMPONENT1_H_
+#define _COMPONENT1_H_
+
+#include "icomponent.h"
+
+class Component1 : public IComponent {
+public:
+	void func();
+};
+
+#endif
---
+++ Sample707/h/component2.h
@@ -0,0 +1,11 @@
+#ifndef _COMPONENT2_H_
+#define _COMPONENT2_H_
+
+#include "icomponent.h"
+
+class Component2 : public IComponent {
+public:
+	void func();
+};
+
+#endif
